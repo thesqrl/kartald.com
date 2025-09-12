d9dba9f4b29c41976cb7d6d5b4d640f07158e513
Dogancan Kartal <dogancankartal@gmail.com>
2025-09-13 00:23:36 +0200

    implement minimal kartald

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,4 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-defineBackend({
-  auth,
-  data,
-});
+defineBackend({});
